39bf49ffd6790e16218dda098d668597a172186a
slowlyo <slowlyo_email@qq.com>
2024-08-06 09:35:13 +0800

    feat: 处理发版时间时区问题

--- buildChangelog.js
+++ buildChangelog.js
@@ -6,7 +6,7 @@ const formatDate = (isoDateStr) => {
     const year = date.getFullYear()
     const month = (date.getMonth() + 1).toString().padStart(2, '0')
     const day = date.getDate().toString().padStart(2, '0')
-    const hours = date.getHours().toString().padStart(2, '0')
+    const hours = (date.getHours() + 8).toString().padStart(2, '0')
     const minutes = date.getMinutes().toString().padStart(2, '0')
     const seconds = date.getSeconds().toString().padStart(2, '0')
 
